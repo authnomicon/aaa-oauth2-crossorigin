212b5f7f30ce323f1cc61fc5e6d9f800bee8228a
Jared Hanson <jaredhanson@gmail.com>
2017-04-04 14:44:30 -0700

    Add stuff.

--- app/handlers/authenticate.js
+++ app/handlers/authenticate.js
@@ -1,8 +1,10 @@
-exports = module.exports = function(Tokens, cors, parseCookies, parse, csrfProtection, errorLogging) {
+exports = module.exports = function(Tokens, cors, parseCookies, parse, csrfProtection, authenticate, errorLogging) {
   
   function respond(req, res, next) {
     // TODO:
     
+    console.log('################################');
+    console.log(req.user);
     console.log(req.headers);
     console.log(req.query);
     
@@ -14,18 +16,33 @@ exports = module.exports = function(Tokens, cors, parseCookies, parse, csrfProte
     console.log('CSRF TOKEN IS:');
     console.log(req.csrfToken())
   
+    // TODO: Check if we have a cookie header with a previous XSRF.  If so, don't need
+    //       cotb
     
     var ctx = {};
-    ctx.user = { id: '1234' };
+    ctx.user = req.user;
     ctx.client = { id: '1' };
     ctx.audience = [ {
       id: 'http://localhost/co/authorize',
       secret: 'some-secret-shared-with-oauth-authorization-server'
     } ];
-    ctx.csrfToken = req.csrfToken();
+    
+    
+    if (req.session.csrfSecret) {
+      // TODO: Expand detection of existing csrf ability
+      ctx.csrfToken = req.csrfToken();
+    } else {
+      ctx.confirmation = {
+        method: 'cotb',
+        origin: req.headers.origin,
+        challenge: req.body.cotb_challenge,
+        transform: 'none'
+      }
+    }
+    
     
     Tokens.cipher(ctx, { type: 'application/jwt', dialect: 'urn:ietf:params:oauth:token-type:id_token' }, function(err, token) {
-      if (err) { return cb(err); }
+      if (err) { return next(err); }
       
       console.log(token);
       
@@ -37,8 +54,14 @@ exports = module.exports = function(Tokens, cors, parseCookies, parse, csrfProte
   return [
     cors({ credentials: true, origin: 'http://localhost:3001' }),
     parseCookies(),
-    parse('application/x-www-form-urlencoded'),
-    csrfProtection({ ignoreMethods: ['POST'] }),
+    parse('application/json'),
+    csrfProtection({ ignoreMethods: [ 'POST' ] }),
+    function(req, res, next) {
+      console.log('******');
+      console.log(req.session);
+      next();
+    },
+    authenticate('local'),
     respond,
     errorLogging()
   ];
@@ -50,5 +73,6 @@ exports['@require'] = [
   'http://i.bixbyjs.org/http/middleware/parseCookies',
   'http://i.bixbyjs.org/http/middleware/parse',
   'http://i.bixbyjs.org/http/middleware/csrfProtection',
+  'http://i.bixbyjs.org/http/middleware/authenticate',
   'http://i.bixbyjs.org/http/middleware/errorLogging'
 ];
