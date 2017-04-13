0ce394f3042a28dbc4f747ff28db07268f5a2502
Jared Hanson <jaredhanson@gmail.com>
2017-04-13 10:03:42 -0700

    Remove unused code.

--- app/handlers/session/challenge.js
+++ app/handlers/session/challenge.js
@@ -4,24 +4,13 @@
 // or yes because this iframe is needed when the password is posted back to the host domain
 // XHR is needed to pass password to AS
 
-exports = module.exports = function(initialize, csrfProtection, Tokens) {
+exports = module.exports = function(csrfProtection, Tokens) {
   var path = require('path')
     , ejs = require('ejs');
   
   
-  function validateToken(req, res, next) {
     // TODO: Validate request
     //if (!req.query.id_token_hint) { return next(new SkipSessionInitiationError()); }
-    
-    
-    
-    Tokens.decipher(req.query.token, { dialect: 'urn:ietf:params:oauth:token-type:id_token' }, function(err, claims, issuer) {
-      if (err) { return next(err); }
-      
-      req.locals.challenge = claims.confirmation[0].challenge;
-      next();
-    });
-  }
   
   function respond(req, res, next) {
     res.locals.csrfToken = req.csrfToken();
@@ -36,15 +25,12 @@ exports = module.exports = function(initialize, csrfProtection, Tokens) {
   
   
   return [
-    initialize(),
     csrfProtection(),
-    validateToken,
     respond
   ];
 };
 
 exports['@require'] = [
-  'http://i.bixbyjs.org/http/middleware/initialize',
   'http://i.bixbyjs.org/http/middleware/csrfProtection',
   'http://i.bixbyjs.org/tokens'
 ];
