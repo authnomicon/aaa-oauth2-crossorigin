3450004201b509df8b5e8ef0564e80dc005c5482
Jared Hanson <jaredhanson@gmail.com>
2017-05-10 13:56:54 -0700

    Add assembly metadata.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,12 @@
+/*
 exports = module.exports = {
   'ext/crossorigin': require('./ext/crossorigin'),
   'tokens/login-ticket/interpret': require('./tokens/login-ticket/interpret'),
   'tokens/login-ticket/translate': require('./tokens/login-ticket/translate')
 };
+*/
 
-exports.load = function(id) {
+exports = module.exports = function(id) {
   try {
     return require('./' + id);
   } catch (ex) {
